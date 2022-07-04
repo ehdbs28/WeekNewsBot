076abe4b4a310905bf1ca7dac7239d92d36293b7
ehdbs28 <doyun5373@naver.com>
2022-07-04 20:31:35 +0900

    2022/07/04
    !내정보 기초

--- bot.js
+++ bot.js
@@ -9,20 +9,27 @@ const client = new Client({
 
 client.once('ready', ()=>{
     console.log("디스코드 봇이 준비되었습니다.");
-    client.user.setActivity("!정보", {type:'PLAYING'});
+    client.user.setActivity("!도움말", {type:'PLAYING'});
 });
 
 client.on('message', message => {
     if(message.content == "!도움말"){
         const embed = new MessageEmbed();
         embed.setTitle("!도움말");
-        embed.setColor("579cfb");
+        embed.setColor("FFB2F5");
         embed.setDescription("\n**!도움말**\n```도움말창을 보여줍니다.```\n\n**!내정보**\n```사용자의 정보를 보여줍니다.```");
         message.channel.send({embeds: [embed]});
     }
 
     if(message.content == "!내정보"){
-        message.channel.send(message.author.displayAvatarURL(ImageData));
+        //message.channel.send(message.author.displayAvatarURL(ImageData));
+        const embed = new MessageEmbed();
+        embed.setTitle("**사용자 정보**");
+        embed.setColor("FFB2F5");
+        embed.setDescription("내 정보");
+        embed.addField(name = "이름", value = "> " + message.author.username, inline = false);
+        //embed.addField(name = "아바타", value = , inline = false);
+        message.reply({embeds : [embed]});
     }
 
 });
