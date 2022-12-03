758a66d1b61e387500984713d58a305f3cb89a01
도윤 <doyun5373@naver.com>
2022-12-03 23:35:29 +0900

    Play song

--- Music bot/bot.js
+++ Music bot/bot.js
@@ -1,24 +1,86 @@
-// const { Client, Intents, Message, MessageEmbed, DiscordAPIError, Options } = require('discord.js');
-// const client = new Client({
-//     intents : [
-//         Intents.FLAGS.GUILDS,
-//         Intents.FLAGS.GUILD_MESSAGES,
-//     ]
-// });
-
 const Discord = require('discord.js');
-const client = new Discord.Client({intents: ["Guilds", "GuildMessages", "MessageContent"]});
+const client = new Discord.Client({intents: ["Guilds", "GuildVoiceStates", "GuildMembers", "GuildMessages", "MessageContent"]});
+const joinVoiceChannel = require('@discordjs/voice');
+
+const ytdl = require('ytdl-core');
+
+const PREFIX = '>';
+
+var servers = {};
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', msg => {
-    console.log(msg.content);
-    if(msg.content == "ping"){
-        
-        msg.reply("pong!");
-    }
+    let args = msg.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'play':
+
+            async function play(connection, msg){
+                var server = servers[msg.guild.id];
+
+                const stream = ytdl(server.queue[0], {filter: "audioonly"})
+                const player = joinVoiceChannel.createAudioPlayer();
+                const resource = joinVoiceChannel.createAudioResource(stream);
+
+                await player.play(resource);
+                connection.subscribe(player);
+
+                server.dispatcher = player;
+
+                server.dispatcher.on(joinVoiceChannel.AudioPlayerStatus.Playing, () => {
+                    console.log("오디오 재생중");
+                })
+                
+                server.queue.shift();
+                
+                server.dispatcher.off(!joinVoiceChannel.AudioPlayerStatus.Playing, () => {
+                    console.log('노래 끝');
+                    if(server.queue[0]){
+                        play(connection, msg);
+                    }
+                    else{
+                        connection.disconnect();
+                    }
+                });
+            }
+
+            if(!args[1]){
+                msg.reply(":no_entry_sign: 링크를 입력해야합니다");
+                return;
+            }
+
+            if(!msg.member.voice.channel){
+                msg.reply(":no_entry_sign: 먼저 보이스 채널에 접속하세요");
+                return;
+            }
+
+            if(!servers[msg.guild.id]) servers[msg.guild.id] = {
+                queue: []
+            }
+
+            var server = servers[msg.guild.id];
+            server.queue.push(args[1]);
+
+            const voiceConnection = joinVoiceChannel.joinVoiceChannel({
+                channelId: msg.member.voice.channelId,
+                guildId: msg.guildId,
+                adapterCreator: msg.guild.voiceAdapterCreator,
+            })
+
+            play(voiceConnection, msg);
+
+            const embed = new Discord.EmbedBuilder()
+            .setTitle("🎵 노래 재생 시작")
+            .setColor('000000')
+            .setDescription(args[1]);
+
+            msg.reply({ embeds : [embed]});
+
+            break;
+    }      
 });
 
 //Login to client
